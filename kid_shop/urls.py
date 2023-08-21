86174b55d1318dc9fa6b36aefbd58d1747525779
gajendra <davegk59@gmil.com>
2023-08-21 17:32:18 +0530

    create sign in authentication

--- kid_shop/urls.py
+++ kid_shop/urls.py
@@ -20,8 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static 
 from django.contrib import admin
 from django.urls import path,include
-
-from core.views import index,contact
  
  
  #username admin
@@ -29,8 +27,9 @@ from core.views import index,contact
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',index, name='index'),
+    
+    path('',include('core.urls')),
     path('items',include('item.urls')),
-    path('contact/',contact, name='contact'),
+    path('admin/', admin.site.urls),
+    
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
