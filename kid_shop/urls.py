a5bc6429d4cfb2124a49cc2cae53d08da9f001fe
gajendra <davegk59@gmil.com>
2023-08-24 16:46:00 +0530

    added conversations

--- kid_shop/urls.py
+++ kid_shop/urls.py
@@ -29,7 +29,8 @@ from django.urls import path,include
 urlpatterns = [
     
     path('',include('core.urls')),
-    path('items',include('item.urls')),
+    path('inbox/',include('conversation.urls')),
+    path('items/',include('item.urls')),
     path('dashboard/',include('dashboard.urls')),
     path('admin/', admin.site.urls),
     
